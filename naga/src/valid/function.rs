e323b14c903dae8d41addb38f5d722ebf7e541d5
atlas dostal <rodol@rivalrebels.com>
2024-09-27 20:48:02 -0400

    unfuck things

--- naga/src/valid/function.rs
+++ naga/src/valid/function.rs
@@ -1093,10 +1093,7 @@ impl super::Validator {
                                 crate::ImageClass::Storage { format, .. } => {
                                     crate::TypeInner::Vector {
                                         size: crate::VectorSize::Quad,
-                                        scalar: crate::Scalar {
-                                            kind: format.into(),
-                                            width: 4,
-                                        },
+                                        scalar: format.into(),
                                     }
                                 }
                                 _ => {
